111e7ee7b2493379425cf37016e5af77afc21163
Dev <frank@fastmail.org>
2019-07-23 14:19:22 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import exifread
+
+def process_img(path):
+    f = open(path, 'rb')
+    tags = exifread.process_file(f)
+    info = {
+        'Image DateTime': tags.get('Image DateTime', '0'),
+        'GPS GPSLatitudeRef': tags.get('GPS GPSLatitudeRef', '0'),
+        'GPS GPSLatitude': tags.get('GPS GPSLatitude', '0'),
+        'GPS GPSLongitudeRef': tags.get('GPS GPSLongitudeRef', '0'),
+        'GPS GPSLongitude': tags.get('GPS GPSLongitude', '0')
+    }
+    return info
+
+info = process_img('image.jpg')
+print(info)
+
